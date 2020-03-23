ed5125f1f6f6e637d5b4980e008d084eb4de34f1
jusinowicz <jusinowicz@gmail.com>
2020-03-23 11:01:53 -0700

    Changed the covid file into a shiny server

--- covid_shiny/corona1.R
+++ covid_shiny/corona1.R
@@ -1,14 +1,16 @@
 library(tidyverse)
 library(lubridate)
-countries = c("Italy", "Canada", "China", "US")
-cv1=read_csv("time_series_19-covid-Confirmed.csv")
+countries = c("Italy", "Canada", "China", "US","Switzerland")
+#cv1=read_csv("time_series_19-covid-Confirmed.csv")
+cv1=read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv")
 colnames(cv1)[1] = "PS"
 colnames(cv1)[2] = "CR"
 #cv1=as.data.frame(cv1)
 # colnames(cv1)[5:61]=mdy(colnames(cv1)[5:61])
 # rename_at(cv1, cv1[,5:61], mdy(colnames(cv1)[5:61])
+d2 = dim(cv1)[2]
 cv1_ts = cv1 %>%
-	gather(key = day, value = N, colnames(cv1)[5:61]) 
+	gather(key = day, value = N, colnames(cv1)[5:d2]) 
 cv1_ts$day = mdy(cv1_ts$day)
 
 cv1_use = cv1_ts %>% 
@@ -28,4 +30,5 @@ for (n in 1:length(countries)){
 
 cv1_cr150 %>%
 	ggplot(aes( x=day_from, y = N, color= CR) ) +
-	geom_line()
+	geom_line()+
+	geom_point()
---
+++ covid_shiny/server.R
@@ -0,0 +1,77 @@
+library(shiny)
+#==============================================================================
+# This is R code to download and plot the latest Covid-19 trajectories. 
+#==============================================================================
+#Load libraries
+library(tidyverse)
+library(lubridate)
+#==============================================================================
+
+#==============================================================================
+# Plot the infection rates. 
+#==============================================================================
+infection_rates = function (countries) {
+
+	#variables from the UI: 
+	countries = c("Italy", "Canada", "China", "US","Switzerland")
+	
+	#Download the latest data set
+	cv1=read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv")
+	colnames(cv1)[1] = "State"  #Rename the headings
+	colnames(cv1)[2] = "Country"
+
+	d2 = dim(cv1)[2] #Get the length of the max time series
+	#Reshape the data set into a time series:
+	cv1_ts = cv1 %>%
+		gather(key = day, value = N, colnames(cv1)[5:d2]) 
+	cv1_ts$day = mdy(cv1_ts$day)
+
+	#Pull out the countries specified in countries
+	cv1_use = cv1_ts %>% 
+		filter(Country %in%  countries)
+
+	#Sum across states in a country for totals
+	cv1_cr = cv1_use %>%
+		group_by(Country,day )%>% 
+	  	summarise( N = sum(N))
+
+	#Find the day at which reported cases reaches 150
+	cv1_cr150 = subset(cv1_cr, N > 150 & N< 60000) %>%
+		mutate(day_from = as.integer(day))
+
+	#Rescale the time series so that they all start on day_150	
+	for (n in 1:length(countries)){
+		cv1_cr150$day_from[cv1_cr150$CR == countries[n]]  = 
+			subset(cv1_cr150, CR == countries[n])$ day_from - 
+				min(subset(cv1_cr150, CR == countries[n])$ day_from)		
+	}
+
+	#plot with ggplot
+	cv1_cr150 %>%
+		ggplot(aes( x=day_from, y = N, color= Country) ) +
+		geom_line()+
+		geom_point()
+
+}
+
+#==============================================================================
+# Shiny server logic
+#==============================================================================
+# Tell the server how to run this: 
+
+shinyServer( function(input, output) {
+	
+	output$infection_rates = renderPlot({
+		nfection_rates(input$countries)
+		})
+
+	url1 = a("R code", href="https://github.com/jusinowicz/coexistence_shiny_apps/tree/master/lottery")
+	url2 = a("me", href="http://jacobusinowicz.com/")
+
+    output$tag1 <- renderUI({
+      tagList("Go to Jacob Usinowicz's github page for:", url1)
+	})
+	output$tag2 <- renderUI({
+      tagList("More about", url2)
+	})
+})
